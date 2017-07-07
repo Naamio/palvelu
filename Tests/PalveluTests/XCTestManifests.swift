a67e4dc5b82726283f7b8ab13bf8367b779493e9
Phil J. Laszkowicz <phil@fillip.pro>
2017-07-08 01:12:16 +0300

    Fixed all tests

--- Tests/PalveluTests/XCTestManifests.swift
+++ Tests/PalveluTests/XCTestManifests.swift
@@ -3,7 +3,7 @@ import XCTest
 #if !os(macOS)
 public func allTests() -> [XCTestCaseEntry] {
     return [
-        testCase(palveluTests.allTests),
+        testCase(PalveluTests.allTests),
     ]
 }
 #endif
