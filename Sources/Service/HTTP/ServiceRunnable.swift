cb851d0f6c83d822a644f54e09473859cc91ad92
Phil J. Laszkowicz <phil@fillip.pro>
2018-05-21 21:39:33 +0300

    Fixed ServiceRunnable incorrect use of RunMode.

--- Sources/Service/HTTP/ServiceRunnable.swift
+++ Sources/Service/HTTP/ServiceRunnable.swift
@@ -2,12 +2,6 @@ import Kitura
 
 public protocol ServiceRunnable {
 
-    /// Mode at which the server is running. Useful for 
-    /// development purposes as the server can be used as
-    /// an instant feedback agent whilst designing and developing
-    /// aspects of an application.
-    var mode: RunMode { get set }
-
     func addRouter(_ router: Router)
 
     func run(withPort port: Int) throws
